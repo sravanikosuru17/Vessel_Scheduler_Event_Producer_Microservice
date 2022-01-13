f0c33c4637e6ec175c0f5d2bcbf031c9a42e0518
Abhidip Ray <abhidray@in.ibm.com>
2022-01-13 18:24:57 +0530

    Update pom with spring-kafka & implementation in Controller

--- src/main/java/com/example/vesselSchedulerEventProducer/controller/vesselSchedulerEventProducerController.java
+++ src/main/java/com/example/vesselSchedulerEventProducer/controller/vesselSchedulerEventProducerController.java
@@ -5,6 +5,7 @@ import com.example.vesselSchedulerEventProducer.service.Producer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,9 +17,18 @@ public class vesselSchedulerEventProducerController {
 
     @Autowired
     Producer producer;
+    
+	
+	@Autowired KafkaTemplate<String, String> kafkaTemplate;
+	 
+    
+    public static final String TOPIC="POC.vesselschedule.topic.internal.any.v1";
 
     @PostMapping(path="/publishData", consumes = "application/json", produces = "application/json")
     public ResponseEntity<String> publishDetails(@RequestBody Data data){
+    	
+    	kafkaTemplate.send(TOPIC, data.toString());
+    	
         String eventId = producer.publishToTopic(data);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventId);
     }
