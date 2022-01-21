dc813b8456ce39fbcfaa2d1f5e9af6d852276b7b
Abhidip Ray <abhidray@in.ibm.com>
2022-01-21 14:57:40 +0530

    Confluent Kafka changes commented to local Kafka

--- src/main/java/com/example/vesselSchedulerEventProducer/config/KafkaProducerConfig.java
+++ src/main/java/com/example/vesselSchedulerEventProducer/config/KafkaProducerConfig.java
@@ -18,25 +18,31 @@ public class KafkaProducerConfig {
     @Value("${spring.kafka.properties.bootstrap.servers}")
     private String bootstrapServers;
 
-	/*
-	 * @Bean public Map<String, Object> producerConfigs() { Map<String, Object>
-	 * props = new HashMap<>(); props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,
-	 * bootstrapServers); props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
-	 * StringSerializer.class);
-	 * props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
-	 * StringSerializer.class); return props; }
-	 */
-
+	
+	@Bean
+	public Map<String, Object> producerConfigs() {
+		Map<String, Object> props = new HashMap<>();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+		return props;
+	}
+	 
+	
     @Bean
     public ProducerFactory<String, String> producerFactory() {
-        return new DefaultKafkaProducerFactory<>(
-        		Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
-        				ProducerConfig.RETRIES_CONFIG, 0,
-        				ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432,
-        				ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
-        				ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class)
-        		);
+		/*
+		 * return new DefaultKafkaProducerFactory<>(
+		 * Map.of(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers,
+		 * ProducerConfig.RETRIES_CONFIG, 0, ProducerConfig.BUFFER_MEMORY_CONFIG,
+		 * 33554432, ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+		 * ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class) );
+		 */
+        
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
     }
+    
+    
 
     @Bean
     public KafkaTemplate<String, String> kafkaTemplate() {
