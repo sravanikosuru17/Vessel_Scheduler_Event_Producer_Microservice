ac06e55a07e71230dc7624ed18f33f5bd99c8ef8
0035R0744 <Amruta.Mali@ibm.com>
2022-01-25 11:52:09 +0530

    added swagger specifications

--- src/main/java/com/example/vesselSchedulerEventProducer/controller/vesselSchedulerEventProducerController.java
+++ src/main/java/com/example/vesselSchedulerEventProducer/controller/vesselSchedulerEventProducerController.java
@@ -1,12 +1,12 @@
 package com.example.vesselSchedulerEventProducer.controller;
 
-import com.example.vesselSchedulerEventProducer.exception.BadRequestException;
-import com.example.vesselSchedulerEventProducer.exception.ServiceException;
 import com.example.vesselSchedulerEventProducer.model.Data;
 import com.example.vesselSchedulerEventProducer.service.KafkaService;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
 
 @RestController
 @RequestMapping(path="/api/v1")
@@ -15,6 +15,7 @@ public class vesselSchedulerEventProducerController {
     @Autowired
     KafkaService kafkaService;
 
+    @ApiOperation("API to Publish data on Kafka")
     @PostMapping(path="/publishData", consumes = "application/json", produces = "application/json")
     @ResponseBody
     @ResponseStatus(HttpStatus.CREATED)
